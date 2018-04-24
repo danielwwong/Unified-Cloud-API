acd7e7040189d632e92943b7b1da5d7f0b751d00
Zhanfeng Huang <vipyellowbee@gmail.com>
2018-04-24 07:45:29 -0400

    Revert "Chaos"
    This reverts commit dc006f14c9cf509b2cd8a38b4b8db1a9006b5281.

--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,10 @@
 from TaskQueue import TaskQueue
 import threading
 import time
-import custom_api
 # import random
 
 class scheduler():
-    def __init__(self, num_workers=1):
+    def __init__(self, num_workers=3):
         self.google_queue = TaskQueue(-1,"Google",num_workers=num_workers)
         self.azure_queue = TaskQueue(-1,"Azure",num_workers=num_workers)
         self.amazon_queue = TaskQueue(-1,"Amazon",num_workers=num_workers)
@@ -15,20 +14,11 @@ class scheduler():
         g_size = self.google_queue.qsize()
         m_size = self.azure_queue.qsize()
         a_size = self.amazon_queue.qsize()
-        print "push"
-        print str(g_size) + " " + str(m_size) + " " + str(a_size) + " "
         if (g_size < m_size and g_size < a_size):
-            print "in Google"
-            task["platform"] = "Google"
             self.google_queue.put(task)
-        # elif m_size <= a_size and m_size <= g_size:
         elif m_size < a_size:
-            print "in Azure"
-            task["platform"] = "Azure"
             self.azure_queue.put(task)
         else:
-            print "in AWS"
-            task["platform"] = "AWS"
             self.amazon_queue.put(task)
 
 
@@ -40,4 +30,21 @@ class scheduler():
     def input(self, mission, path, filename, platform, bucket_name):
         self.push_task(self.task2Json(mission, path, filename, platform, bucket_name))
 
-
+# m_list = ["uplaod","download","delete"]
+# p_list = ["google","amazon","azure"]
+# path_list = [".//f:", "e:backup", "cd:could up"]
+# b_list = ["bucket_1", "bucket_2", "bucket_3"]
+#
+# s = scheduler()
+#
+# for i in range(3):
+#     s.push_task(s.task2Json(m_list[ random.randint(0, 2) ],path_list[random.randint(0, 2)],
+#                                   str(i)+".png" ,p_list[random.randint(0, 2)],b_list[random.randint(0, 2)]), block=True, timeout=None)
+#     s.push_task(s.task2Json(m_list[ random.randint(0, 2) ],path_list[random.randint(0, 2)],
+#                                   str(i)+".png" ,p_list[random.randint(0, 2)],b_list[random.randint(0, 2)]), block=True, timeout=None)
+#     s.push_task(s.task2Json(m_list[ random.randint(0, 2) ],path_list[random.randint(0, 2)],
+#                                   str(i)+".png" ,p_list[random.randint(0, 2)],b_list[random.randint(0, 2)]), block=True, timeout=None)
+#
+# print("***************leader:wait for finish!")
+# # q.join()  # wait for finishing
+# print("***************leader:all task finished!")
